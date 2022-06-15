10ee67838775ce35b88f6cf82dc7f6dac1fd4624
Kevin Barnard <kbarnard@mbari.org>
2022-06-15 11:19:14 -0700

    Set ROV:training-set group tag for new annotations

--- util/m3.py
+++ util/m3.py
@@ -190,7 +190,8 @@ def create_observation(video_reference_uuid, concept, observer,
         'video_reference_uuid': video_reference_uuid,
         'concept': concept,
         'observer': observer,
-        'activity': 'localize'
+        'activity': 'localize',
+        'group': 'ROV:training-set'
     }
 
     if not (timecode or elapsed_time_millis or recorded_timestamp):
